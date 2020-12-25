2522c3fb956118db77fa617c7cb08a8c8e370fb2
Marcus <marcus.kaiser.1990@gmail.com>
2020-12-25 21:27:04 +0000

    update spearman ranks

--- independence_tests/corr/pearson.py
+++ independence_tests/corr/pearson.py
@@ -2,18 +2,23 @@ from scipy.linalg import norm
 from scipy.special._ufuncs import btdtr
 
 
+def _p_value(r, num_samples):
+    r = max(min(r, 1.0), -1.0)
+
+    # Calculate beta distribution with both parameters equal to n / 2 - 1:
+    shape_param = 0.5 * num_samples - 1.0
+    p = 2 * btdtr(shape_param, shape_param, 0.5 * (1.0 - abs(r)))
+    return p
+
+
 def pearson(x, y):
     """
     Fast implementation of Pearson Correlation. Expects clean data.
     Implementation follows ideas from ``scipy.stats.pearsonr``.
     """
-    x_ = x - x.mean()
-    y_ = y - y.mean()
+    x = x - x.mean()
+    y = y - y.mean()
 
-    r = x_.dot(y_) / norm(x_) / norm(y_)
-    r = max(min(r, 1.0), -1.0)
-
-    # Calculate beta distribution with both parameters equal to n / 2 - 1:
-    shape_param = 0.5 * x.shape[0] - 1.0
-    p = 2 * btdtr(shape_param, shape_param, 0.5 * (1.0 - abs(r)))
+    r = x.dot(y) / norm(x) / norm(y)
+    p = _p_value(r, x.shape[0])
     return r, p
--- independence_tests/corr/spearman.py
+++ independence_tests/corr/spearman.py
@@ -1,6 +1,7 @@
 import numpy as np
+from scipy.linalg import norm
 
-from independence_tests.corr.pearson import pearson
+from independence_tests.corr.pearson import _p_value
 
 
 def fast_rank(x, y):
@@ -8,16 +9,34 @@ def fast_rank(x, y):
     arg_sort = np.empty_like(x, dtype=np.int_)
 
     arg_sort[:] = x.argsort()
-    x_arg_rev = np.empty_like(arg_sort)
+    x_arg_rev = np.empty_like(arg_sort, dtype=np.float_)
     x_arg_rev[arg_sort] = np.arange(len(x))
 
     arg_sort[:] = y.argsort()
-    y_arg_rev = np.empty_like(arg_sort)
+    y_arg_rev = np.empty_like(arg_sort, dtype=np.float_)
     y_arg_rev[arg_sort] = np.arange(len(y))
     return x_arg_rev, y_arg_rev
 
 
 def spearman(x, y):
-    """ Fast implementation of Spearman Correlation. Expects clean data. """
+    """
+    Fast implementation of Spearman Correlation. Expects clean data.
+    Note: This does not handle repeated data points. The results diverge
+    from the "classical" ``scipy.stats.spearmanr`` implementation with
+    averaged ranks for multiple occurrences of variable values
+    (e.g. constant arrays).
+    """
     x_rank, y_rank = fast_rank(x, y)
-    return pearson(x_rank, y_rank)
+
+    # calculate mean and standard-deviation for uniform distribution:
+    mean = (x_rank.shape[0] - 1.0) / 2.0
+    std = ((x_rank.shape[0] ** 2 - 1) / 12) ** 0.5
+
+    x_rank -= mean
+    x_rank /= std
+    y_rank -= mean
+    y_rank /= std
+
+    r = x_rank.dot(y_rank) / x.shape[0]
+    p_value = _p_value(r, x.shape[0])
+    return r, p_value
